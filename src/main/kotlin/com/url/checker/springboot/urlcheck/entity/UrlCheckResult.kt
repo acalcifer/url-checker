9aff93af277989a9b2950407434362226ff9c7a8
alexgolubev <alex.golubev0303@gmail.com>
2024-10-24 23:33:38 +0200

    Refactor application.properties to yml, move to const request config variables in UrlCheckService.kt

--- src/main/kotlin/com/url/checker/springboot/urlcheck/entity/UrlCheckResult.kt
+++ src/main/kotlin/com/url/checker/springboot/urlcheck/entity/UrlCheckResult.kt
@@ -11,7 +11,7 @@ import java.util.UUID
 @Entity
 data class UrlCheckResult(
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.UUID)
     val id: UUID = UUID.randomUUID(),
 
     @Column(nullable = false)
